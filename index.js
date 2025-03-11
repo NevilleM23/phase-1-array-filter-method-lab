65d78d7890cee6ee8e8daf92081b4c9be48c6e2e
Nevillemuli <neville.muli@gmail.com>
2025-03-11 23:14:49 +0300

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() ===  name.toLowerCase())    
+}
+
+function fuzzyMatch(drivers, firstLetter){
+    return drivers.filter(driver => driver.startsWith(firstLetter))
+} 
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name )
+}
